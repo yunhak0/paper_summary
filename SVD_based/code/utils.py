9dacebbc09d0f30baabcd8b8ae878c5cd7791c80
Yunhak Oh <yunhak.oh@gmail.com>
2021-09-01 20:45:31 +0900

    [SVD++][ADD] Add `get_index()` for csr_matrix

---
+++ SVD_based/code/utils.py
@@ -0,0 +1,23 @@
+from tqdm import tqdm
+from scipy import sparse
+
+def get_index(rating: sparse.csr_matrix) -> dict:
+    """Get Indexes of Rating Matrix
+
+    Args:
+        rating (sparse.csr_matrix): rating matrix with csr_matrix format
+
+    Returns:
+        dict: keys are user, item, data_idx
+        user is index or id of user.
+        item is index or id of item.
+        data_idx is index of csr_matrix.data
+    """    
+    dict_idx = {'user': [], 'item':[], 'data_idx': []}
+    for u, (begin, end) in tqdm(enumerate(zip(rating.indptr,
+                                              rating.indptr[1:])),
+                                total=len(rating.indptr)):
+        dict_idx['user'].append(u)
+        dict_idx['item'].append(list(rating.indices[begin:end]))
+        dict_idx['data_idx'].append(list(range(begin, end)))
+    return dict_idx
